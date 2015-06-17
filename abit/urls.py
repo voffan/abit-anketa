c4d8878ac6cc85cf492732d9fff155a6f1aabd5b
st-pi <anrebrov@mail.ru>
2015-06-17 16:06:23 +0900

    url zabil

--- abit/urls.py
+++ abit/urls.py
@@ -3,25 +3,27 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from anketa.views import StartPage, StartApp, Streets, Territory, District, Zipcode, City, Settlement
+from anketa.views import StartPage, StartApp, Streets, Territory, District, City, Citizenship, Nation, DocIssuer, PrevEduName, Institute, EduProg, EduProf, Privilegies, Rank
 
 from anketa.views2 import *
 
 urlpatterns = patterns('',
     url(r'^$', StartPage.as_view()),
     url(r'^application/$', StartApp),
-    url(r'^streets/$', Streets, name='streets'),
     url(r'^territory/$', Territory, name = 'territory'),
     url(r'^district/$', District, name = 'district'),
     url(r'^city/$', City , name = 'city'), 
-    url(r'^settlement/$', Settlement, name = 'settlement'),
-    url(r'^zipcode/$', Zipcode, name = 'zipcode'),
-    #url(r'^streets/$',Streets,name='streets'),
-    #url(r'^feedback/$', FeedbackCreateView.as_view()),
-    #url(r'^ajax/categ/$', 'anketa.views.feeds_subcat'),
-    #url(r'^ajax/autocomplete/$', 'anketa.views.autocomplete'),
+    url(r'^streets/$', Streets, name='streets'),
+    url(r'^citizenship/$', Citizenship, name = 'citizenship'),
+    url(r'^nation/$', Nation, name = 'nation'),
+    url(r'^docissuer/$', DocIssuer, name = 'docissuer'),
+    url(r'^preveduname/$', PrevEduName, name = 'preveduname'),
+    url(r'^institute/$', Institute, name = 'institute'),
+    url(r'^eduprog/$', EduProg, name = 'eduprog'),
+    url(r'^eduprof/$', EduProf, name = 'eduprof'),
+    url(r'^privilegies/$', Privilegies, name = 'privilegies'),
+    url(r'^rank/$', Rank, name = 'rank'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^staff/', include('staff.urls',namespace = 'staff')),
-    #url(r'^appwizard/', Application_Wizard.as_view([Step_Name, Step_Document, Step_Education, Step_Address])),
     url(r'^auth/', include('staff.urls')),
 )
