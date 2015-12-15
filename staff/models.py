a7a2deed7b1d7e0e5b1c3c1bb2ef851dd8e91c89
4ino <vovka_rp@mail.ru>
2015-12-16 02:24:38 +0900

    new model'ka

--- staff/models.py
+++ staff/models.py
@@ -13,21 +13,12 @@ def imagepath(instance, filename):
 class Employee(models.Model):
     user = models.ForeignKey(User, verbose_name=u'Пользователь', db_index=True)
     department = models.ForeignKey(Department,verbose_name=u'Подразделение')
-    #position = models.ForeignKey(UniEmployees, verbose_name=u'Должность')
     position = models.ForeignKey('Position', verbose_name=u'Должность')
-    ufirst_name = models.ForeignKey('UniEmployees', verbose_name=u'Фамилия', db_index=True)
-    ulast_name = models.ForeignKey('UniEmployees', verbose_name=u'Имя', db_index=True)
-    umiddle_name = models.ForeignKey('UniEmployees', verbose_name=u'Отчество', db_index=True)
+    uniempl = models.ForeignKey('UniEmployees', verbose_name=u'УнивСотрудник', db_index=True)
     fullname = models.CharField(u'ФИО', max_length = 300, db_index=True)
 <<<<<<< HEAD
     def __unicode__(self):
         return self.fullname
-
-class EmplContacts(models.Model):
-    employee = models.ForeignKey(Employee, verbose_name=u'ФИО')
-    #contact_type = models.ForeignKey(Attribute, verbose_name=u'Тип контакта')
-    #value = models.CharField(u'Контакт', max_length = 150, db_index=True)
-    value = models.ForeignKey(AttrValue, verbose_name=u'Значение', db_index=True)
 
 class Position(models.Model):
     name = models.Charfield(u'Должность',max_length=200, db_index=True)
@@ -37,18 +28,17 @@ class UniEmployees(models.Model):
     first_name = models.Charfield(u'Фамилия', max_length=100, db_index=True)
     last_name = models.Charfield(u'Имя', max_length = 100, db_index=True)
     middle_name = models.Charfield(u'Отчество', max_length = 100, db_index = True)
-    #position = models.Charfield(u'Должность', max_length =200)
 =======
     position = models.CharField(u'Должность', max_length=200)
-    
+
     def __str__(self):
         return self.fullname
 
 class EmplContacts(models.Model):
     employee = models.ForeignKey(Employee, verbose_name=u'Сотрудник', db_index=True)
-    contact_type = models.ForeignKey(AttrValue, verbose_name=u'Тип контакта')
+    contact_type = models.ForeignKey(Attribute, verbose_name=u'Тип контакта')
     value = models.CharField(u'Контакт', max_length = 150, db_index=True)
-    
+
     def __str__(self):
         return self.employee.fullname+' '+value
 >>>>>>> 050749fd77a0ee07e5c21a51b107ebeb212156ae
@@ -59,11 +49,11 @@ class News(models.Model):
     Description = models.CharField(u'Описание', max_length=100)
     NewsDate = models.DateField(u'Дата новости', db_index=True)
     NewsText = models.TextField(u'Контент')
-    
+
     def __str__(self):
         return self.NewsName
 
-    
+
 class Img(models.Model):
     news = models.ForeignKey(News, verbose_name=u'Название новости')
     img  = models.ImageField(upload_to= imagepath, verbose_name=u'Имя', help_text='150x150px')
