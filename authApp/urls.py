7dc1afe54e320f42f83d569dbf619dc4a27e3ae3
st-pi <anrebrov@mail.ru>
2016-03-19 01:41:37 +0900

    Register html

--- authApp/urls.py
+++ authApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$',views.login_web, name = 'login'),
-    url(r'^checkuser',views.checkUser, name='checkuser'),
+    url(r'^checkUserValid',views.checkUserValid, name='checkUserValid'),
+    url(r'^checkPersonValid',views.checkPersonValid, name='checkPersonValid'),
     url(r'^register/$', views.register_index, name="register_index")
 ]
